0ca70eb6712aeeec4128073a5ce3759b128f657b
Régis Behmo <regis@behmo.com>
2013-12-04 18:31:51 +0100

    Post dates are not functional, yet

--- prodigal/filters.py
+++ prodigal/filters.py
@@ -9,6 +9,18 @@ def filter(fn):
 def pouac(value):
     return value + " Pouac!"
 
+TEMPLATE_DATES = {}
+@filter
+def set_date(template_name, date):
+    global TEMPLATE_DATES
+    TEMPLATE_DATES[template_name] = date
+
+@filter
+def latest_pages(count):
+    dates = [(d, n) for n, d in TEMPLATE_DATES.iteritems()]
+    dates.sort(reverse=True)
+    return [d[1] for d in dates[:count]]
+
 def register_all(env):
     # List all functions with @filter decorator
     module = sys.modules[__name__]
